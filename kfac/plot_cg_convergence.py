d9b966f8f2ebe13df9da4e165345e8d4fd8f43f7
Nikolaos Tselepidis <ntselepidis@student.ethz.ch>
2022-06-14 00:22:25 +0200

    Add script for plotting convergence of CG

---
+++ kfac/plot_cg_convergence.py
@@ -0,0 +1,65 @@
+import argparse
+import os
+import matplotlib.font_manager as font_manager
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file_id', default=249, type=int)
+    parser.add_argument('--logscale', default=0, choices=[0, 1], type=int)
+    parser.add_argument('--stop_iter', default=-1, type=int)
+    parser.add_argument('--output', default='png', choices=['pdf', 'png'])
+    parser.add_argument('--dpi', default=300)
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = get_args()
+
+    filename = 'cg_benchmark/iter-' + str(args.file_id) + '.csv'
+    print(f'Reading {filename} ...')
+    df = pd.read_csv(filename)
+
+    if args.stop_iter > 0:
+        df = df[df['iter'] <= args.stop_iter]
+
+    #df = df[~(df['prec'].str.contains('m1|m2|kfac-m3|Qb'))]
+
+    drop_precs = [
+        #'none',
+        #'kfac',
+        #'kfac-cgc',
+        'kfac-cgc-m1',
+        'kfac-cgc-m2',
+        #'kfac-cgc-m3',
+        #'kfac-m3',
+        'kfac-m2',
+        'kfac-cgc-m1-Qb',
+        'kfac-cgc-m2-Qb',
+        #'kfac-cgc-m3-Qb',
+        #'kfac-m3-Qb',
+        'kfac-m2-Qb',
+        ]
+
+    for drop_prec in drop_precs:
+        df = df[~(df['prec'] == drop_prec)]
+
+    sns.set_style("darkgrid")
+
+    fig, axs = plt.subplots(1, 2, figsize=(2*6.4, 4.8))
+    fig.suptitle(f'conjgrad convergence plots ( step = {args.file_id} )')
+
+    sns.lineplot(ax=axs[0], data=df, x="iter", y="val", hue="prec")
+    sns.lineplot(ax=axs[1], data=df, x="iter", y="relres", hue="prec")
+
+    if args.logscale:
+        axs[1].set_yscale('log')
+
+    plt.savefig(f'iter-{args.file_id}.{args.output}', dpi=args.dpi)
+
+if __name__ == '__main__':
+    main()
