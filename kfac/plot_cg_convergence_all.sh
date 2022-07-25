d832b0d0db414bd3f9d9397009e7087422d2a15d
Nikolaos Tselepidis <ntselepidis@student.ethz.ch>
2022-07-25 17:32:35 +0200

    Modify `plot_cg_convergence_all.sh`
    This script now generates CG convergence plots
    for every directory under `rootdir`

--- kfac/plot_cg_convergence_all.sh
+++ kfac/plot_cg_convergence_all.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-dir=$1
+rootdir=$1
 
 stop_iter=150
 logscale=0
 
-for f in `ls $dir`; do
-    echo "Generating plot for $dir/$f ..."
-    python plot_cg_convergence.py --filename $dir/$f --logscale $logscale --stop_iter $stop_iter
+for dir in `ls $rootdir`; do
+    echo $dir
+    for f in `ls $rootdir/$dir`; do
+        echo "Generating plot for $rootdir/$dir/$f ..."
+        python plot_cg_convergence.py --filename $rootdir/$dir/$f --logscale $logscale --stop_iter $stop_iter
+    done
 done
